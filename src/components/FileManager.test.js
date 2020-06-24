fd5375c9383bda3fedaa31e9f985579166196af1
Francisco <franciscorios0610@gmail.com>
2020-06-23 20:01:05 -0500

    test v2

--- src/components/FileManager.test.js
+++ src/components/FileManager.test.js
@@ -1,16 +1,42 @@
 import React from 'react'
+import Enzyme, { mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 import { FileManager } from './FileManager'
 
+Enzyme.configure({ adapter: new Adapter() })
+
 describe('<FileManager />', () => {
-  const component = (
+  const wrapper = mount(
     <FileManager
       domain='https://www.responsivefilemanager.com'
-      target='image'
-      isOpen={false}
+      target='cover'
+      isOpen
     />
   )
 
-  it('renders', () => {
-    expect(component).not.toBeNull()
+  it('renders without crashing', () => {
+    expect(wrapper).not.toBeNull()
+  })
+
+  it('pathdialog should be {`filemanager/dialog.php`} by default', () => {
+    expect(wrapper.props().pathdialog).toBe('filemanager/dialog.php')
+  })
+
+  it('type should be {2} by default', () => {
+    expect(wrapper.props().type).toBe(2)
+  })
+
+  it('width should be {`100%`} by default', () => {
+    expect(wrapper.props().width).toBe('100%')
+  })
+
+  it('height should be {500} by default', () => {
+    expect(wrapper.props().height).toBe(500)
+  })
+
+  it('allows us to set props', () => {
+    expect(wrapper.props().domain).toBe('https://www.responsivefilemanager.com')
+    wrapper.setProps({ domain: 'http://devfran.com' })
+    expect(wrapper.props().domain).toBe('http://devfran.com')
   })
 })
--- src/hooks/useFileManager.test.js
+++ src/hooks/useFileManager.test.js
@@ -1,7 +1,31 @@
+import React from 'react'
+import Enzyme, { mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 import { useFileManager } from './useFileManager'
 
+Enzyme.configure({ adapter: new Adapter() })
+
 describe('useFileManager()', () => {
-  it('is truthy', () => {
-    expect(useFileManager).toBeTruthy()
+  let hook
+  const HookWrapper = () => {
+    hook = useFileManager({ setFieldValue: () => {} })
+    return null
+  }
+  mount(<HookWrapper />)
+
+  it('isOpen should be false by default', () => {
+    expect(hook.isOpen).toBe(false)
+  })
+
+  it('isOpen should be target by default', () => {
+    expect(hook.target).toBe('')
+  })
+
+  it('handleFilemanager is truthy', () => {
+    expect(hook.handleFilemanager).toBeTruthy()
+  })
+
+  it('toggle is truthy', () => {
+    expect(hook.toggle).toBeTruthy()
   })
 })
